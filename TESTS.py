c3faece67e7132c1fafeb72d347242723f0650a9
Alex <regoner2@gmail.com>
2022-03-24 12:28:51 +0200

    tests

---
+++ TESTS.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+class test_distributions(unittest.TestCase):
+    def test_binominal_distribution(self0):
+        pass
+    def test_poisson_distribution(self):
+        pass
+    def test_hyperGeometric_distribution(self):
+        pass
+    def test_bernolli_distribution(self):
+        pass
+    def test_uniform_distribution(self):
+        pass
+    def test_Geometric_distribution(self):
+        pass
+
+
+
--- plot.py
+++ plot.py
@@ -5,13 +5,13 @@ import matplotlib.pylab as plt
 
 import distributions
 
+number_of_tries = 100000
+size_sample_space = 100
 probabilities = {}
 successes = {}
 values = []
 
-
 plt.xlabel('Value of the random variable ')
-plt.ylabel('Amount of successfull tries (out of 100000)')
 
 
 def generate_values(n):
@@ -23,7 +23,7 @@ def generate_values(n):
     return values
 
 
-def generate_probabilities(dist, N=0, D=0, n=0, p=0, gamma=0.0):
+def generate_probabilities(dist, N=0, D=0, n=0, p=0.0, gamma=0.0):
     global probabilities
     probabilities = {}
     if dist == "poisson":
@@ -57,25 +57,67 @@ def generate_successes(n: int):
                 successes[value] += 1
 
 
+def plot_binom(N=100, p=0.7777):
+    global number_of_tries
+    generate_probabilities("binom", N=N, p=p)
+    plt.title("Binominal distribution with N= " + str(N) + " with probabillity " + str(p))
+    generate_successes(number_of_tries)
+
+
+def plot_poisson(gamma=0.6):
+    global number_of_tries
+    generate_probabilities("poisson", gamma=gamma)
+    plt.title("Poisson distribution with gamma= " + str(gamma))
+    generate_successes(number_of_tries)
+
+
+def plot_uniform():
+    global number_of_tries
+    generate_probabilities("uni")
+    plt.title("Uniform distribution ")
+    generate_successes(number_of_tries)
+
+
+def plot_bernolli(p=0.7):
+    global number_of_tries
+    generate_probabilities("bernolli", p=p)
+    plt.title("Bernoulli distribution with probabillity " + str(p))
+    generate_successes(number_of_tries)
+
+
+def plot_geom(p=0.5):
+    global number_of_tries
+    generate_probabilities("geom", p=p)
+    plt.title("Geometric distribution with probabillity " + str(p))
+    generate_successes(number_of_tries)
+
+
+def plot_hypGeom(N=100, D=50, n=40):
+    global number_of_tries
+    generate_probabilities("hyp", N=N, D=D, n=n)
+    plt.title("HyperGeometric distribution with N= " + str(N) + " D= " + str(D) + " n= " + str(n))
+    generate_successes(number_of_tries)
+
+
 def main():
+    global size_sample_space
     args = sys.argv[1:]
-    generate_values(100)
-    # generate_probabilities("hyp", N= 100, D= 50, n= 40)
-    # generate_probabilities("binom", N= 100, p= 0.7777)
-    # generate_probabilities("poisson", gamma= 0.6)
-    # generate_probabilities("poisson",gamma= 20)
-    # generate_probabilities("uni")
-    # generate_probabilities("geom", p= 0.5)
-    generate_probabilities("bernolli" , p = 0.7)
-    generate_successes(100000)
-    suc =  list(successes.values())
+    generate_values(size_sample_space)
+    # plot_uniform()
+    # plot_geom()
+    # plot_hypGeom()
+    plot_binom()
+    # plot_bernolli()
+    # plot_poisson()
+    suc = list(successes.values())
     print(values)
     print(list(probabilities.values()))
     print(suc)
     plt.plot(values, suc)
+
+    plt.ylabel('Amount of successfull tries (out of ' + str(number_of_tries) + ')')
     plt.show()
+
 
 if __name__ == '__main__':
     main()
-
-
